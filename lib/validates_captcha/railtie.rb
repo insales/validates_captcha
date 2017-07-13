19941f3febc93425452d7b1696ea9abb6754969c
dborisov <scorpion061181@gmail.com>
2017-07-13 12:54:38 +0300

    fix gem loading in specs

--- lib/validates_captcha/railtie.rb
+++ lib/validates_captcha/railtie.rb
@@ -1,14 +1,12 @@
 module ValidatesCaptcha
   class Railtie < Rails::Railtie
     initializer 'validates_captcha.configure' do |app|
-      app.middleware.use ::ValidatesCaptcha::MiddlewareWrapper
-    end
-
-    config.after_initialize do
       ::ActiveRecord::Base.send :include, ValidatesCaptcha::ModelValidation
       ::ActionController::Base.send :include, ValidatesCaptcha::ControllerValidation
       ::ActionView::Base.send :include, ValidatesCaptcha::FormHelper
       ::ActionView::Helpers::FormBuilder.send :include, ValidatesCaptcha::FormBuilder
+
+      app.middleware.use ::ValidatesCaptcha::MiddlewareWrapper
     end
 
     rake_tasks do
